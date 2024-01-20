7925eb51614f2643acfe74855f2be4c521fc1961
Alejo <jesus.torres.atelier@gmail.com>
2024-01-20 23:55:47 +0100

    fixing css and html structure

--- src/View/Profile/profile.php
+++ src/View/Profile/profile.php
@@ -1,18 +1,131 @@
-<h1 class="profileTitle">Votre profil</h1>
-<?php
-    include_once 'userData.php';
+<div class="banner-div">
+    <div class="banner-image"></div>
+</div>
+<main class="mainContainer">
+    <div id="detail-profil">
+        <div class="photo-profile"></div>
+        <div class="username">Y QUE PENSAIS DE UN NOMBRE DE UN USUARIO TAN LARGO COMO ESTO?</div>
+        <div class="name">JESUS ALEJANDRO TORRES MARTINIELLO SE VOLVIO FULLSTACK MADRE MIA</div>
+        <div class="familyName">TORRES MARTINIELLO DE LA VEIRA COJONES ME CAGO EN TU</div>
+        <div class="role">Role</div>
+        <div class="date-inscription">Date d'inscription</div>
+        <div class="description-du-profile">There isn't even a description here but why not</div>
+        <button id="edit-profile-button">Modifier le profil</button>
+        <div class="logout-section">
+            <button class="logout-button">
+                <span class="logout">LOG OUT</span>
+            </button>
+        </div>
+    </div>
+    <div class="content-section">
+        <div class="nav-barre">
+            <a href="" class="nav-link nav-Naturothèques">Mes naturotheques</a>
+            <a href="" class="nav-link nav-Register">Register</a>
+            <a href="" class="nav-link nav-Creation">Creation de naturotheques</a>
+        </div>
 
-    echo '<p>Your ID : ' . $GLOBALS['userId'] . ' </p>';
-    echo '<p>Your Username : ' . $GLOBALS['username'] . ' </p>';
-    echo '<p>Your Mail : ' . $GLOBALS['userMail'] . ' </p>';
-    echo '<p>Your Role : ' . $GLOBALS['userRole'] . ' </p>';
-    echo '<p>Your Birthdate : ' . $GLOBALS['userBirthDate'] . ' </p>';
-    echo '<p>Your Surname : ' . $GLOBALS['userSurname'] . ' </p>';
-    echo '<p>Your Family Name : ' . $GLOBALS['userFamilyName'] . ' </p>';
-    echo '<p>Your Phone Number : ' . $GLOBALS['userPhoneNumber'] . ' </p>';
-    echo '<p>Your Domain : ' . $GLOBALS['userDomain'] . ' </p>';
-    ?>
-<p><a href="Profile/Disconnect">Disconnect</a></p>
-<p><a href="Profile/Settings">Settings</a></p>
-<p><a href="Profile/Password">Change password</a></p>
-<p><a href="Profile/DeleteAccount">Delete account</a></p>
+        <div class="list-grid">
+            <div class="libraryUnit">
+                <div class="imageSpace">
+                    <div class="backgroundLock">
+                        <div class="boutonLock">
+                            <div id="img"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="name-library">Nom naturo</div>
+            </div>
+            <div class="libraryUnit">
+                <div class="imageSpace">
+                    <div class="backgroundLock">
+                        <div class="boutonLock">
+                            <div id="img"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="name-library">Nom naturo</div>
+            </div>
+            <div class="libraryUnit">
+                <div class="imageSpace">
+                    <div class="backgroundLock">
+                        <div class="boutonLock">
+                            <div id="img"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="name-library">Nom naturo</div>
+            </div>
+            <div class="libraryUnit">
+                <div class="imageSpace">
+                    <div class="backgroundLock">
+                        <div class="boutonLock">
+                            <div id="img"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="name-library">Nom naturo</div>
+            </div>
+            <div class="libraryUnit">
+                <div class="imageSpace">
+                    <div class="backgroundLock">
+                        <div class="boutonLock">
+                            <div id="img"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="name-library">Nom naturo</div>
+            </div>
+            <div class="libraryUnit">
+                <div class="imageSpace">
+                    <div class="backgroundLock">
+                        <div class="boutonLock">
+                            <div id="img"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="name-library">Nom naturo</div>
+            </div>
+            <div class="libraryUnit">
+                <div class="imageSpace">
+                    <div class="backgroundLock">
+                        <div class="boutonLock">
+                            <div id="img"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="name-library">Nom naturo</div>
+            </div>
+            <div class="libraryUnit">
+                <div class="imageSpace">
+                    <div class="backgroundLock">
+                        <div class="boutonLock">
+                            <div id="img"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="name-library">Nom naturo</div>
+            </div>
+            <div class="libraryUnit">
+                <div class="imageSpace">
+                    <div class="backgroundLock">
+                        <div class="boutonLock">
+                            <div id="img"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="name-library">Nom naturo</div>
+            </div>
+            <div class="libraryUnit">
+                <div class="imageSpace">
+                    <div class="backgroundLock">
+                        <div class="boutonLock">
+                            <div id="img"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="name-library">Nom naturo</div>
+            </div>
+        </div>
+    </div>
+
+</main>
