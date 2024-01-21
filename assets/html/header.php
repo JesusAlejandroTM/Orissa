c59a199c8f33a59b0062a50134371deae25486a0
BOZLAK Esma Nur <145288399+Estimatee@users.noreply.github.com>
2024-01-21 18:20:11 +0100

    Update header.php
    ajout de div, ajout de la bar de navigation plus js

--- assets/html/header.php
+++ assets/html/header.php
@@ -1,36 +1,69 @@
 <header>
-    <a href="/Orissa" class="logo"><img src="Orissa/../assets/img/orissa-logo.png" alt="logo" /></a>
-    <ul>
-        <li><a href="Library">LIBRARY</a></li>
-        <li><a href="Search">TAXON</a></li>
-        <li><a href="">DISCOVER</a></li>
-        <li><a href="Home">ABOUT</a></li>
-    </ul>
-    <div class="search">
-        <a href="Search" class="searchIcon">
-            <span>
-                <ion-icon name="search-outline" class="searchBtn"></ion-icon>
-            </span>
-        </a>
-        <?php
-            use App\Code\Lib\UserSession;
-            if (UserSession::isConnected()) {
-                $link = 'Profile';
-                $icon = 'people-outline';
-            }
-            else {
-                $link = 'Login';
-                $icon = 'log-in-outline';
-            }
+    <div class="nav-container">
 
-            echo '
+        <div class="navbar">
+            <a href="/Orissa" class="logo"><img src="Orissa/../assets/img/orissa-logo.png" alt="logo" /></a>
+
+            <!-- Move the mobile menu toggle inside the navbar -->
+            <div class="menu-toggle" id="mobile-menu">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </div>
+
+            <ul class="nav-menu">
+                <li><a href="Library" class="nav-links">LIBRARY</a></li>
+                <li><a href="Search" class="nav-links">TAXON</a></li>
+                <li><a href="" class="nav-links">DISCOVER</a></li>
+                <li><a href="Home" class="nav-links">ABOUT</a></li>
+            
+
+
+              <li><div class="search">
+                <a href="Search" class="searchIcon">
+                    <span>
+                        <ion-icon name="search-outline" class="searchBtn"></ion-icon>
+                    </span>
+                </a></li>
+               
+                <?php
+
+                use App\Code\Lib\UserSession;
+
+                if (UserSession::isConnected()) {
+                    $link = 'Profile';
+                    $icon = 'people-outline';
+                } else {
+                    $link = 'Login';
+                    $icon = 'log-in-outline';
+                }
+
+                echo '
             <a href="' . $link . '">
             <span>
                 <ion-icon name="' . $icon . '" class="searchBtn"></ion-icon>
             </span>
             </a>';
-        ?>
-    </div>
+                ?>
+            </div>
+        </div>
+   
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+
+    <script>
+        const menu =document.querySelector('#mobile-menu');
+        const menuLinks = document.querySelector('.nav-menu');
+      
+        menu.addEventListener('click', function() {
+            menu.classList.toggle('is-active');
+            menuLinks.classList.toggle('active');
+        });
+
+    
+    
+
+    </script>
+    </div>
+    </div>
 </header>
